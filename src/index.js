059d0062b94d62621c939a81040910410051b222
simaoDeveloperOFC <simaopereira953@gmail.com>
2026-05-14 15:06:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// A linha do index.css foi removida daqui
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
